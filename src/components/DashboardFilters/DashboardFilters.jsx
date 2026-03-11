4837552890eb2920b3be878c3263b0f959f1c5c1
Nathan Mufuta <nathan.mufuta-mulamba@etu.univ-amu.fr>
2026-03-11 13:23:14 +0100

    Refactorisation du composant Dashboard

---
+++ src/components/DashboardFilters/DashboardFilters.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+function DashboardFilters({ onLeagueChange }) {
+  return (
+    <div className="d-flex flex-column flex-md-row justify-content-between align-items-md-center gap-3 mb-5">
+
+      {/* Slider dates */}
+      <div className="filter-group d-flex/components/ gap-1">
+        <button className="filter-btn">Hier</button>
+        <button className="filter-btn active">Aujourd'hui</button>
+        <button className="filter-btn">Demain</button>
+      </div>
+
+      {/* Sort */}
+      <div className="sort-container d-flex align-items-center gap-2">
+
+        <span className="material-symbols-outlined fs-5">
+          sort
+        </span>
+
+        <select
+          className="custom-select"
+          onChange={onLeagueChange}
+          defaultValue=""
+        >
+          <option value="" disabled hidden>
+            Sort by: League
+          </option>
+
+          <option value="CL">Champions League</option>
+          <option value="PL">Premier League</option>
+          <option value="PD">La Liga</option>
+          <option value="FL1">Ligue 1</option>
+
+        </select>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default DashboardFilters;
