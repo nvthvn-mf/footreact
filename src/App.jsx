618aa073f4d3de8306c2ea8046e1cfe952bf242b
nvthvn-mf <nathanmf06@gmail.com>
2026-03-11 15:33:17 +0100

    Correction de l'alignement de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
 import React from 'react';
 import './App.css';
-import MatchCard from './components/MatchCard/MatchCard';
-import LeagueStandings from './components/Standings/LeagueStandings.jsx';
-import TopScorer from './components/TopScorer/TopScorer';
 import Home from './components/Home/Home.jsx';
 import Sidebar from "./components/SideBar/Sidebar.jsx";
 
 function App() {
-  
   return (
-
-      <div className="d-flex min-vh-100" style={{ backgroundColor: 'var(--color-bg-dark)' }}>
+      // vh-100 et overflow-hidden verrouillent la taille de la fenêtre
+      <div className="d-flex vh-100 overflow-hidden" style={{ backgroundColor: 'var(--color-bg-dark)' }}>
 
           <Sidebar />
 
-          <main className="flex-grow-1" style={{ overflowY: 'auto' }}>
+          {/* Le main prend tout l'espace restant, sans scroller lui-même */}
+          <main className="flex-grow-1 d-flex">
               <Home />
           </main>
 
       </div>
   );
-
 }
 
 export default App;
