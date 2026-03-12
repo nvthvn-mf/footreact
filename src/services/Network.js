019d313c960cd6641cc7c160f5928c3c11e53ec6
nvthvn-mf <nathanmf06@gmail.com>
2026-03-12 11:04:17 +0100

    Ajout du service réseau

---
+++ src/services/Network.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const API_KEY='e54c0687314746b882f10e330625734b';
+const API_URL='https://api.football-data.org/v4';
+
+const apiClient = axios.create({
+    baseURL : API_URL,
+    headers: {
+        'X-Auth-Token': API_KEY,
+        //'X-Auth-'
+        'Content-Type': 'application/json',
+    },
+    timeout: 10000,
+});
+
+export const get = async (endpoint) => {
+    try {
+        const response = await apiClient.get(endpoint);
+        return response.data
+    } catch (error){
+        if (error.response){
+            console.error(`Erreur Serveur (${error.response.status}) sur ${endpoint}:`, error.response.data);        }
+        else if (error.request){
+            console.error(`Erreur Réseau sur ${endpoint} - Pas de réponse du serveur`);
+        }
+        else {
+            console.error(`Erreur d'exécution sur ${endpoint}:`, error.message);
+        }
+        throw error;
+    }
+}
