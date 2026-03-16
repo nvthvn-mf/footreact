89d61a9bc14a9aae2bb7bf46ae27e1234fc37799
nvthvn-mf <nathanmf06@gmail.com>
2026-03-16 09:09:54 +0100

    Mise à jour TopScorer Ligue 1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,15 +7,11 @@ import QuickTips from './QuickTips/QuickTips.jsx';
 
 function Home() {
   return (
-      // d-flex permet d'aligner les enfants, flex-xl-row les met côte à côte sur ordi
       <div className="home-container d-flex flex-column flex-xl-row">
 
-          {/* Colonne Centrale : Dashboard (prend l'espace restant) */}
           <div className="home-dashboard-area">
               <Dashboard />
           </div>
-
-          {/* Colonne de Droite : Insights (Taille fixe de 350px) */}
             <aside className="home-right-sidebar d-none d-xl-flex flex-column gap-5 p-4 p-xl-5">              <TopScorer />
               <QuickTips />
               <LeagueStandings />
