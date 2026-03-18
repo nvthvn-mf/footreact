00d0cfc105af268dbbacf7c51dfc29ce048ed2f9
Kyliann <kouassi-jean-marie-k.KOUAME@etu.univ-amu.fr>
2026-03-18 08:43:45 +0100

    résolution du problème JNaN dans le composant CompetitionMatches.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,10 @@
-// Les imports de base (React, hooks, librairies essentielles)
 import React from 'react';
 
-// Tes composants et modules
-// Composants parents
 import Dashboard from './Dashboard/Dashboard.jsx';
 import LeagueStandings from './Standings/LeagueStandings.jsx';
 import TopScorer from './TopScorer/TopScorer.jsx';
 import QuickTips from './QuickTips/QuickTips.jsx';
 
-// Les styles et assets
 import './Home.css';
 
 
@@ -19,7 +15,8 @@ function Home() {
           <div className="home-dashboard-area">
               <Dashboard />
           </div>
-            <aside className="home-right-sidebar d-none d-xl-flex flex-column gap-5 p-4 p-xl-5">              <TopScorer />
+            <aside className="home-right-sidebar d-none d-xl-flex flex-column gap-5 p-4 p-xl-5">
+              <TopScorer />
               <QuickTips />
               <LeagueStandings />
           </aside>
